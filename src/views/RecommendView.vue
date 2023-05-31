7fb2982f6bb53e1b3e933bf2a25218c87e79d189
niyuancheng <nyc20010820@126.com>
2023-05-31 13:58:33 +0800

    update：完成推荐电影的模块

--- src/views/RecommendView.vue
+++ src/views/RecommendView.vue
@@ -1,15 +1,30 @@
 <template >
   <div class="recommend-container">
     <h1>电影推荐</h1>
-
+    <div class="recommendMovies-container">
+      <div class="movie-item" v-for="(item, index) in recommendMovies" :key="index">
+        <img :src="imgUrl" alt="已评分电影" loading="lazy">
+        <div class="movie-itemTitle">{{ item.title }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 
 <script setup>
-import { ref } from 'vue'
-import {request} from "axios";
+import axios from "axios"
+import { ref } from "vue"
+
+const recommendMovies = ref([]);
+axios.get("http://localhost:8888/api/movie/getRecommend", {
+  params: {
+    userId: window.localStorage.getItem("userId")
+  }
+}).then(res => {
+  console.log(res.data);
+  recommendMovies.value = res.data.data
+})
 
 </script>
 
@@ -18,10 +33,46 @@ h1 {
   text-align: center;
   font-weight: 600;
   font-size: 23px;
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
   padding: 10px;
   color: #000;
   border-radius: 5px;
+}
+
+.recommendMovies-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.movie-item {
+  position: relative;
+  background-color: #fff;
+  border-radius: 10px;
+  max-width: 190px;
+  height: 300px;
+  margin-top: 20px;
+  margin-left: 10px;
+  transition: all .5s ease;
+  border-radius: 10px;
+  cursor: pointer;
+}
+img  {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+}
+.movie-item:hover {
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+}
+.movie-itemTitle {
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-weight: 600;
+  background-color: rgba(0,0,0,0.5);
+  color: #fff;
 }
 </style>
 
