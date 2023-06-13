273449fc2214b2750db1c617bd1d8bc0cd62f81d
niyuancheng <nyc20010820@126.com>
2023-06-13 18:27:39 +0800

    删去无用的依赖

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import router from './router'
 import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import * as echarts from "echarts";
-//import {Vue} from "vue-demi";
 
-//Vue.prototype.$echarts=echarts;
 const app = createApp(App)
 app.use(router).use(ElementPlus).mount('#app')
 
